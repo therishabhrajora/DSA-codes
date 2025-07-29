434c2c06498b9ba8c7c36ae95ea253872865378d
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-07-29 22:54:31 +0530

    Node classes

---
+++ NodeDll.java
@@ -0,0 +1,11 @@
+public class NodeDll {
+    int data;
+    NodeDll next;
+    NodeDll prev;
+
+    NodeDll(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
